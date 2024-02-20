31d44d9b403b2192f84767d6368ee6ba941b8a65
Jeff McDonald <jwmcdonald85@gmail.com>
2024-02-20 13:51:21 -0500

    created search option

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,24 @@
 
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 function Footer(props) {
-  return (
-    <div className="footer-container">
-        <div className="footer">
-            <h5>
-                <a href="https://github.com/pisces7diana">| Diana |</a>
-                <a href="https://github.com/geno223"> Geno |</a>
-                <a href="https://github.com/rabhioli"> Rabhi |</a>
-                <a href="https://github.com/jeffmcd21"> Jeff 
-                    <i class="fa-solid fa-biohazard fa-fade"></i>|</a>
+    return (
+        <div className="footer-container">
+            <div className="footer">
+                <h5>
+                    <a href="https://github.com/pisces7diana">| Diana |</a>
+                    <a href="https://github.com/geno223"> Geno |</a>
+                    <a href="https://github.com/rabhioli"> Rabhi |</a>
+                    <a href="https://github.com/jeffmcd21"> Jeff 
+                        <i class="fa-solid fa-biohazard fa-fade"></i>|</a>
                     <h6>Click for GitHub Repo</h6>
-            </h5>
-      <Link to="/">
-            <button>Return to Index</button>
-      </Link>
+                </h5>
+                {/* <Link to="/">
+                    <button>Return to Index</button>
+                </Link> */}
+            </div>
         </div>
-    </div>
-  )
-}
+    )
+};
 
 export default Footer
